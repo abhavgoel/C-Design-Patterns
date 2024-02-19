3a78aa657a815114d77180347bb3570920867c1a
Abhav Goel <goelabhav2002@gmail.com>
2024-02-20 01:06:44 +0530

    added comments

--- Behavioural/Command/main.cpp
+++ Behavioural/Command/main.cpp
@@ -1,68 +1,89 @@
 #include<bits/stdc++.h>
 
-struct Character{
+// Define a Character structure with x, y coordinates and a name
+struct Character {
     int x;
     int y;
     std::string name;
-    Character(std::string _name){
-        name=_name;
-    }
-    void move(int _x,int _y){
+
+    // Constructor to initialize the character with a name
+    Character(std::string _name) : name(_name) {}
+
+    // Function to move the character to a new position
+    void move(int _x, int _y) {
         x = _x;
         y = _y;
     }
 };
 
-class Command{
-    public:
-    virtual ~Command(){}
-    virtual void execute(Character& c)=0;
-    virtual void undo(Character& c)=0;
+// Base class representing a command
+class Command {
+public:
+    virtual ~Command() {}
 
+    // Abstract functions for executing and undoing a command
+    virtual void execute(Character& c) = 0;
+    virtual void undo(Character& c) = 0;
 };
 
-class Move: public Command{
-    private:
+// Derived class representing a move command
+class Move : public Command {
+private:
     int x;
     int y;
-    public:
-    void execute(Character& c){
+
+public:
+    // Function to execute a move command
+    void execute(Character& c) override {
         int randx = rand() % 4;
         int randy = rand() % 4;
-        c.move(randx,randy);
-        x=randx;
-        y=randy;
-        std::cout<<c.name<<" moved "<<x<<", "<<y<<"\n";
+        c.move(randx, randy);
+        x = randx;
+        y = randy;
+        std::cout << c.name << " moved " << x << ", " << y << "\n";
     }
-    void undo(Character& c){
-        c.move(-x,-y);
-        std::cout<<c.name<<" undo "<<-x<<" , "<<-y<<"\n";
+
+    // Function to undo a move command
+    void undo(Character& c) override {
+        c.move(-x, -y);
+        std::cout << c.name << " undo " << -x << " , " << -y << "\n";
     }
 };
 
-int main()
-{
+int main() {
+    // Create a Character named Kratos
     Character Kratos("Kratos");
-    std::vector<Command*>command_queue;
 
+    // Create a vector to store pointers to Command objects
+    std::vector<Command*> command_queue;
+
+    // Create Move objects and add them to the command queue
     Move* m1 = new Move;
     command_queue.push_back(m1);
+
     Move* m2 = new Move;
     command_queue.push_back(m2);
+
     Move* m3 = new Move;
     command_queue.push_back(m3);
+
     Move* m4 = new Move;
     command_queue.push_back(m4);
 
-
-   for(int i=0;i<command_queue.size();i++)
-   {
+    // Execute each command in the command queue
+    for (int i = 0; i < command_queue.size(); i++) {
         command_queue[i]->execute(Kratos);
-   }
-   for(int i=command_queue.size()-1;i>=0;i--)
-   {
-    command_queue[i]->undo(Kratos);
-   }
+    }
+
+    // Undo each command in reverse order
+    for (int i = command_queue.size() - 1; i >= 0; i--) {
+        command_queue[i]->undo(Kratos);
+    }
+
+    // Cleanup: delete dynamically allocated Move objects
+    for (Command* cmd : command_queue) {
+        delete cmd;
+    }
 
     return 0;
 }
