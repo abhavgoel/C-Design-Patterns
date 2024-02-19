8f0299ddd7b3a4b705002070600b902143698af4
Abhav Goel <goelabhav2002@gmail.com>
2024-02-20 01:04:46 +0530

    Command pattern updated

--- Behavioural/Command/main.cpp
+++ Behavioural/Command/main.cpp
@@ -1,9 +1,16 @@
 #include<bits/stdc++.h>
-using namespace std;
 
 struct Character{
     int x;
     int y;
+    std::string name;
+    Character(std::string _name){
+        name=_name;
+    }
+    void move(int _x,int _y){
+        x = _x;
+        y = _y;
+    }
 };
 
 class Command{
@@ -15,17 +22,47 @@ class Command{
 };
 
 class Move: public Command{
+    private:
+    int x;
+    int y;
     public:
     void execute(Character& c){
-
+        int randx = rand() % 4;
+        int randy = rand() % 4;
+        c.move(randx,randy);
+        x=randx;
+        y=randy;
+        std::cout<<c.name<<" moved "<<x<<", "<<y<<"\n";
     }
     void undo(Character& c){
-
+        c.move(-x,-y);
+        std::cout<<c.name<<" undo "<<-x<<" , "<<-y<<"\n";
     }
 };
 
 int main()
 {
-    Move m;
+    Character Kratos("Kratos");
+    std::vector<Command*>command_queue;
+
+    Move* m1 = new Move;
+    command_queue.push_back(m1);
+    Move* m2 = new Move;
+    command_queue.push_back(m2);
+    Move* m3 = new Move;
+    command_queue.push_back(m3);
+    Move* m4 = new Move;
+    command_queue.push_back(m4);
+
+
+   for(int i=0;i<command_queue.size();i++)
+   {
+        command_queue[i]->execute(Kratos);
+   }
+   for(int i=command_queue.size()-1;i>=0;i--)
+   {
+    command_queue[i]->undo(Kratos);
+   }
+
     return 0;
 }
